31c910e21c775acaec58071f295abfaf9fc0b664
brunocordioli072 <brunocordioli072@gmail.com>
2020-11-09 23:06:37 -0300

    commit

--- setup.py
+++ setup.py
@@ -1,29 +1,22 @@
 import json
 import pandas as pd
 from core import MatrixUtils
-from scipy.spatial import distance_matrix
+from scipy.spatial import distance
 import numpy as np
 mu = MatrixUtils()
+mu
+with open("./resources/data.json", "r") as read_file:
+    data = json.load(read_file)
 
-with open("./resources/random_vertices.json", "r") as read_file:
-        data = json.load(read_file)
-
-vertices = data["vertices"]
+vertices = data["matrix"]
 cities = data["cities"]
+
+matrix = distance.cdist(vertices, vertices)
 print("=============================================")
 print("Distance Matrix Example: \n")
-print(mu.formatMatrix(mu.getDistanceMatrix(vertices), cities))
+print(mu.formatMatrix(matrix, cities))
 print("\n=============================================")
 
-with open("./resources/random_matrix.json", "r") as read_file:
-        data = json.load(read_file)
-
-rand_matrix = np.array(data["matrix"])
-rand_index = data["cities"]
-
 print("Routing Matrix: \n")
-print("Existing Random Matrix: \n")
-print(mu.formatMatrix(rand_matrix, rand_index))
-
 print("\nRouting Matrix Result: \n")
-print(mu.formatMatrix(mu.getRoutingMatrix(rand_matrix), rand_index))
+print(mu.formatMatrix(mu.getRoutingMatrix(matrix), cities))
